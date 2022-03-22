85986df5047388c338fc13090da6a1d244668628
Alan Nadolny <alannadolnyy@gmail.com>
2022-03-22 21:17:30 +0100

    add sending request

--- frontend/src/ui/UserForm.js
+++ frontend/src/ui/UserForm.js
@@ -6,7 +6,6 @@ import { SignIn, Register } from '../ducks/user/operation';
 
 function UserForm({ SignIn, Register }) {
   const { action } = useParams();
-
   const schema = yup.object().shape({
     login: yup.string('Login should be a string').required('Login is required'),
     password: yup
@@ -16,33 +15,45 @@ function UserForm({ SignIn, Register }) {
 
   return (
     <div>
-      {action === 'login' ? 'sign in' : 'register'}
-      {/* sign in i register wpakuj w cos jak chcesz, tak zeby pasowalo ci do stylowania */}
-      <Formik
-        validationSchema={schema}
-        onSubmit={(values) => {
-          action === 'login'
-            ? SignIn(values.login, values.password)
-            : Register(values.login, values.password);
-        }}
-        enableReinitialize={true}
-        initialValues={{
-          login: '',
-          password: '',
-        }}
-      >
-        <Form>
-          <div id='errors'>
-            <ErrorMessage name='login' component='div' />
-            <ErrorMessage name='password' component='div' />
+      <div className='main-login'>
+        <div className='login-form'>
+          <div className='login-setting'>
+            <h1 id='login-title'>
+              {' '}
+              {action === 'login' ? 'SIGN IN!' : 'REGISTER!'}{' '}
+            </h1>
+            <Formik
+              validationSchema={schema}
+              onSubmit={(values) => {
+                action === 'login'
+                  ? SignIn(values.login, values.password)
+                  : Register(values.login, values.password);
+              }}
+              enableReinitialize={true}
+              initialValues={{
+                login: '',
+                password: '',
+              }}
+            >
+              <Form>
+                <label>Login: </label>
+                <ErrorMessage id='error' name='login' component='div' />
+                <Field name='login' />
+                <label>Password: </label>
+                <ErrorMessage id='error' name='password' component='div' />
+                <Field name='password' type='password' />
+                <div id='button-container'>
+                  <button type='submit'> SUBMIT </button>
+                </div>
+              </Form>
+            </Formik>
+            <a id='help' href=''>
+              {' '}
+              Do you need help? Contact us!{' '}
+            </a>
           </div>
-          <strong>Login </strong>
-          <Field name='login' />
-          <strong>Password: </strong>
-          <Field name='password' type='password' />
-          <button type='submit'>zaloguj</button>
-        </Form>
-      </Formik>
+        </div>
+      </div>
     </div>
   );
 }
