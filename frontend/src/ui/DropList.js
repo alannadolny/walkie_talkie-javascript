d050a9d3f452e40ad3b0e5cab5d8091fb051670f
Alan Nadolny <alannadolnyy@gmail.com>
2022-03-26 00:12:32 +0100

    add hiding droplist

--- frontend/src/ui/DropList.js
+++ frontend/src/ui/DropList.js
@@ -6,56 +6,88 @@ import list from '../list.png';
 import { getUserFromState } from '../ducks/user/selector';
 import { connect } from 'react-redux';
 
-function DropList({user, Logout, visible, setVisible}){
+function DropList({ user, Logout, visible, setVisible }) {
+  function ShowList() {
+    if (visible) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+  }
 
-    function ShowList(){
-        
-        if(visible){
-            setVisible(false);
-        }else{
-            setVisible(true);
-            }
-        }
-
-    return <div id='header-center-container'>
-                <img id='list' alt='error' onClick={() => ShowList()} src={list} />
-                {visible ? <div id="droplist-container"> 
-                    <nav className='droplist'>
-                                <div id="droplist-element">
-                                    <Link to='/'> HOME </Link>
-                                </div>
-                            
-                                <div id="droplist-element">
-                                    <Link to='/'> ABOUT </Link>
-                                </div>
-                           
-                                <div id="droplist-element">
-                                    <Link to='/'> CONTACT </Link>
-                                </div>
-                            
-                            {
-                            !user.login ?
-                            
-                                <div id="droplist-element">
-                                    <Link to='/form/login'> SIGN IN </Link>
-                                </div>
-                             :  <div id="droplist-element"> <Link to='/' onClick={() => Logout()} >
-                                    LOG OUT
-                                </Link> </div>
-                            }
-                            {!user.login ? <div id="droplist-element">
-                                 <Link to='/form/register'> REGISTER </Link> </div> : ''}
-                    </nav>
-                </div>: ''}
+  return (
+    <div id='header-center-container'>
+      <img id='list' alt='error' onClick={() => ShowList()} src={list} />
+      {visible ? (
+        <div id='droplist-container'>
+          <nav className='droplist'>
+            <div id='droplist-element'>
+              <Link to='/' onClick={() => setVisible(false)}>
+                {' '}
+                HOME{' '}
+              </Link>
             </div>
+
+            <div id='droplist-element'>
+              <Link to='/' onClick={() => setVisible(false)}>
+                {' '}
+                ABOUT{' '}
+              </Link>
+            </div>
+
+            <div id='droplist-element'>
+              <Link to='/' onClick={() => setVisible(false)}>
+                {' '}
+                CONTACT{' '}
+              </Link>
+            </div>
+
+            {!user.login ? (
+              <div id='droplist-element'>
+                <Link to='/form/login' onClick={() => setVisible(false)}>
+                  {' '}
+                  SIGN IN{' '}
+                </Link>
+              </div>
+            ) : (
+              <div id='droplist-element'>
+                {' '}
+                <Link
+                  to='/'
+                  onClick={() => {
+                    Logout();
+                    setVisible(false);
+                  }}
+                >
+                  LOG OUT
+                </Link>{' '}
+              </div>
+            )}
+            {!user.login ? (
+              <div id='droplist-element'>
+                <Link to='/form/register' onClick={() => setVisible(false)}>
+                  {' '}
+                  REGISTER{' '}
+                </Link>{' '}
+              </div>
+            ) : (
+              ''
+            )}
+          </nav>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 }
 const mapStateToProps = (state) => {
-    return {
-      user: getUserFromState(state),
-    };
+  return {
+    user: getUserFromState(state),
   };
-  
-  const mapDispatchToProps = {
-    Logout,
-  };
-export default connect(mapStateToProps,mapDispatchToProps)(DropList)
+};
+
+const mapDispatchToProps = {
+  Logout,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(DropList);
--- frontend/src/ui/Main.js
+++ frontend/src/ui/Main.js
@@ -1,7 +1,5 @@
-function Main(){
-    return <div>
-        
-    </div>
+function Main() {
+  return <div></div>;
 }
 
 export default Main;
