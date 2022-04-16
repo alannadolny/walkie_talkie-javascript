2203f9530aa9cf086d8803cf58cb4bdd8fd1f707
Pawkooo13 <strzalecki9@wp.pl>
2022-04-16 21:19:34 +0200

    filter owners name

--- frontend/src/ui/ChannelFilters.js
+++ frontend/src/ui/ChannelFilters.js
@@ -1,12 +1,34 @@
+import { connect } from 'react-redux';
+import { getChannelsFromState } from '../ducks/channels/selector';
+import {GetChannelList} from '../ducks/channels/operation';
+import { useEffect } from 'react';
+import * as _ from 'lodash';
 
-function ChannelFilters() {
+function ChannelFilters({channels,GetChannelList}) {
   let prevf = 0;
   let currf = 0;
   let currs = 0;
   let prevs = 0;
   let checkboxlist = [...document.querySelectorAll('input')];
+  let getOwnerList = document.getElementById('owner-name-list');
+  let ownerList = [];
 
+  useEffect(() => {
+    if (_.isEmpty(channels)) GetChannelList();
+  }, []);
+  
+  function SetOwnerList(){
+    getOwnerList.style.display = 'flex';
 
+    channels.forEach(element => {
+      ownerList.push(element.owner[0].login);
+    })
+    ownerList = [...new Set(ownerList)];
+
+    ownerList.forEach(element => {
+      getOwnerList.innerHTML += `<div id='owner-name'> <input type='checkbox'/> <span> ${element} </span> </div>`;
+    })
+  } 
 
   function FiltersSetCheckboxes(){
     prevf=currf;
@@ -15,11 +37,17 @@ function ChannelFilters() {
         currf = i;
       }
     }
-      for(let j=1; j<4; j++){
-        if(j !== currf){
-          checkboxlist[j].checked = false;
-        }
+    for(let j=1; j<4; j++){
+      if(j !== currf){
+        checkboxlist[j].checked = false;
       }
+    }
+    if(checkboxlist[2].checked === true){
+      SetOwnerList();
+    }else{
+      getOwnerList.style.display = 'none';
+      ownerList = ownerList.shift(0,ownerList.length);
+    }
   }
 
   function SortSetCheckboxes(){
@@ -29,15 +57,17 @@ function ChannelFilters() {
         currs = i;
       }
     }
-      for(let j=4; j<6; j++){
-        if(j !== currs){
-          checkboxlist[j].checked = false;
-        }
+    for(let j=4; j<6; j++){
+      if(j !== currs){
+        checkboxlist[j].checked = false;
       }
+    }
   }
 
-  function SortChannelList(){
-    
+  function SortingChannelList(){
+    if(checkboxlist[1].checked === true){
+      
+    }
   }
 
   return (
@@ -48,6 +78,9 @@ function ChannelFilters() {
         <div className='filters'>
           <div> <input type='checkbox' onClick={FiltersSetCheckboxes}/> Channel name </div>
           <div> <input type='checkbox' onClick={FiltersSetCheckboxes}/> Owner name </div>
+          <div id='owner-name-list'> 
+          
+          </div>
           <div> <input type='checkbox' onClick={FiltersSetCheckboxes}/> Active user </div>
         </div>
         <label id='channel-filters-label'> Sort: </label> 
@@ -55,9 +88,22 @@ function ChannelFilters() {
           <div> <input type='checkbox' onClick={SortSetCheckboxes}/> Increasing </div>
           <div> <input type='checkbox' onClick={SortSetCheckboxes}/> Decreasing </div>
         </div>
-        <button id='filter-button' type='submit' onClick={SortChannelList}> Submit </button>
+        <button id='filter-button' type='submit' onClick={SortingChannelList}> Submit </button>
     </div>
   );
 }
 
-export default ChannelFilters;
+//export default ChannelFilters;
+
+
+const mapStateToProps = (state) => {
+  return {
+    channels: getChannelsFromState(state),
+  };
+};
+
+const mapDispatchToProps = {
+  GetChannelList
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelFilters);
