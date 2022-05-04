c0772d465c97dc52de9ece1d7ce337ae6367b9f1
Alan Nadolny <alannadolnyy@gmail.com>
2022-05-05 00:24:04 +0200

    fill about us

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ todo:
 - usuwanie refa jeśli użytkownik wyjdzie z pokoju
 - dodanie wylaczania/wlaczania audio i kamery
 - dodanie uploadowania obrazkow jako awatary
-- dodanie podstron typu: about me
 - dodanie filtrowania kanalow
 
 do stylowania:
 
 - loading page
+- about us
--- frontend/src/ui/About.js
+++ frontend/src/ui/About.js
@@ -1,5 +1,17 @@
 function About() {
-  return <div className='about-main'></div>;
+  return (
+    <div className='about-main'>
+      Welcome to Walkie Talkie, your number of all real-time communicators.
+      We're dedicated to providing you with the very best online communicator,
+      with an emphasis on a stable connection, great audio and voice quality,
+      and fast and always responding servers. Founded in 2022 by two students,
+      Walkie Talkie has come a long way from its beginnings in Gdańsk. When they
+      first started out, their passion for providing online communication drove
+      them to start their own business. We hope you enjoy our products as much
+      as we enjoy offering them to you. If you have any questions or comments,
+      please don't hesitate to contact us. Sincerely, Walkie Talkie Team
+    </div>
+  );
 }
 
 export default About;
